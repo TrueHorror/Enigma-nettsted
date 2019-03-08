95df5d1c20afa6e5470f1fb0131d431946fa9a09
Unknown <fredrik.olsvold@gmail.com>
2019-03-08 12:09:16 +0100

    Begynt med admin loginn
    Start av kode

---
+++ Kode/admin_login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page Title</title>
+  </head>
+  <body>
+  	<form action="admin_panel.php" method="post">
+  		<label></label>
+  		<input type="text" name="brukernavn">
+  		<label></label>
+  		<input type="password" name="passord">
+  	</form>
+
+  </body>
+</html>
