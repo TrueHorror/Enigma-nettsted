32dbcd6a5b5905bf1bb343989a40bc3f5e9cffdf
Unknown <fredrik.olsvold@gmail.com>
2019-03-14 11:38:41 +0100

    Oppdatering på admin innlogging

--- Kode/admin_login.php
+++ Kode/admin_login.php
@@ -4,12 +4,22 @@
     <title>Page Title</title>
   </head>
   <body>
-  	<form action="admin_panel.php" method="post">
-  		<label></label>
-  		<input type="text" name="brukernavn">
-  		<label></label>
-  		<input type="password" name="passord">
-  	</form>
+  	<header>
+  		<nav></nav>
+  	</header>
+  	<main>	
+	  	<form action="admin_panel.php" method="post">
+	  		<label for="brukernavn">Brukernavn</label>
+	  		<input class="admin_input" type="text" name="brukernavn">
 
+	  		<label for="passord">Passord</label>
+	  		<input class="admin_input" type="password" name="passord">
+
+	  		<button id="login_btn" type="submit" name="logginn">Logg inn</button>
+	  	</form>
+    </main>
+    <footer>
+    	
+    </footer>
   </body>
 </html>
---
+++ Kode/data.php
@@ -0,0 +1,7 @@
+<?php
+//admin_brukernavn
+$melk = "Superenigma"
+
+//admin_passord
+$potetgull ="Hemmeligenigmapassord"
+?>
